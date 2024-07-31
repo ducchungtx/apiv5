8e3088e6d68959d490bbb7781d1cfb342fe4c359
Chung Nguyen <zrmedia9@gmail.com>
2024-07-31 21:42:49 +0700

    feat: Add crawl-brand-links, crawl-manual-links, and crawl-manual-detail API endpoints
    This commit adds three new API endpoints: crawl-brand-links, crawl-manual-links, and crawl-manual-detail. These endpoints allow users to crawl and retrieve specific information from different URLs.
    
    The crawl-brand-links endpoint fetches and returns a list of links related to a specific brand.
    
    The crawl-manual-links endpoint fetches and returns a list of links to manuals, along with additional information such as description and manufacturer.
    
    The crawl-manual-detail endpoint fetches and returns detailed information about a specific manual, including the title and a link to the PDF file.
    
    These new endpoints enhance the functionality of the post-document API and provide users with more options for retrieving data from external sources.

--- src/api/post-document/controllers/post-document.ts
+++ src/api/post-document/controllers/post-document.ts
@@ -5,10 +5,27 @@
 import { factories } from '@strapi/strapi'
 
 export default factories.createCoreController('api::post-document.post-document', ({ strapi }) => ({
-  async crawlLinks(ctx) {
-    const { url } = ctx.query;
-    const { postDocument } = strapi.services;
-    const links = await postDocument.crawlLinks(url);
-    ctx.send(links);
+  async 'crawl-brand-links'(ctx) {
+    const url = ctx.query.url;
+    const links = await strapi.service('api::post-document.post-document').crawlBrandLinks(url);
+    return {
+      links
+    };
+  },
+  async 'crawl-manual-links'(ctx) {
+    const url = ctx.query.url;
+    console.log('url', url);
+    const links = await strapi.service('api::post-document.post-document').crawlManualLinks(url);
+    return {
+      links
+    };
+  },
+  async 'crawl-manual-detail'(ctx) {
+    const url = ctx.query.url;
+    console.log('url', url);
+    const info = await strapi.service('api::post-document.post-document').crawlManualDetail(url);
+    return {
+      info
+    };
   }
 }));
--- src/api/post-document/routes/custom-post-document.ts
+++ src/api/post-document/routes/custom-post-document.ts
@@ -1,13 +1,19 @@
-import path from "path";
-import { config } from "process";
-import middlewares from "../../../../config/middlewares";
-
 export default {
   routes: [
     {
       method: 'GET',
-      path: '/post-document/crawl-links',
-      handler: 'api::post-document.post-document.crawl-links',
+      path: '/post-document/crawl-brand-links',
+      handler: 'api::post-document.post-document.crawl-brand-links',
+    },
+    {
+      method: 'GET',
+      path: '/post-document/crawl-manual-links',
+      handler: 'api::post-document.post-document.crawl-manual-links',
+    },
+    {
+      method: 'GET',
+      path: '/post-document/crawl-manual-detail',
+      handler: 'api::post-document.post-document.crawl-manual-detail',
     }
   ]
 }
