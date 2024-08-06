da087c1fbd864d468ffe2d8301f74512ec31b298
Chung Nguyen <zrmedia9@gmail.com>
2024-08-06 21:53:44 +0700

    feat: Refactor post-document service
    This commit refactors the post-document service to improve code organization and remove unused dependencies. The axios and cheerio imports have been removed as they are no longer needed. This refactoring enhances the maintainability and clarity of the codebase.

--- src/api/post-document/services/post-document.ts
+++ src/api/post-document/services/post-document.ts
@@ -1,8 +1,6 @@
 /**
  * post-document service
  */
-import axios from 'axios';
-import * as cheerio from 'cheerio';
 import { factories } from '@strapi/strapi';
 
 export default factories.createCoreService('api::post-document.post-document');
